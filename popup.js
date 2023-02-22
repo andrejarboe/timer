028869314b7acdb720f2c0a230df41cc541613b1
Andre Jarboe <andrejarboe@gmail.com>
2023-02-22 01:00:20 -0500

    badge text working

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
 const timeElement = document.getElementById("time")
 const currentTime = new Date().toLocaleTimeString()
 timeElement.textContent = `The time is ${currentTime}`
+
+chrome.action.setBadgeText({
+    text: "TIme",
+}), () => {
+    console.log("Finished setting badge text.");
+}
