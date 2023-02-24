57830a151ef1d17cf9fdcf82f87f3f3a868b7a42
Andre Jarboe <andrejarboe@gmail.com>
2023-02-24 15:39:23 -0500

    log alarm

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
-console.log("Hello from background script!");
+chrome.alarms.create({
+    // 1 is mins
+    // so 1/60 is seconds
+	periodInMinutes: 1 / 60,
+});
 
-let time = 0 
 
-setInterval(() => {
-    time += 1
+chrome.alarms.onAlarm.addListener((alarm) => {
+    console.log(alarm);
+})
 
-    console.log(time);
-}, 1000)
+// Example:
+// console.log("Hello from background script!");
+
+// let time = 0
+
+// setInterval(() => {
+//     time += 1
+
+//     console.log(time);
+// }, 1000)
